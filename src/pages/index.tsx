2c2166ed4415869471b1754e915869c8bcd7ce7d
Victoria Cheng <victoria.cheng15@gmail.com>
2022-10-20 16:02:25 -0600

    feat: add socialLink component to home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,13 @@
 import React from "react";
 import Layout from "@theme/Layout";
 import Hero from "../components/Hero";
+import SocialLinks from "../components/SocialLinks";
 
 function Home(): JSX.Element {
   return (
     <Layout title="Home">
       <Hero />
-      <main>
-        <h2>main</h2>
-      </main>
+      <SocialLinks />
     </Layout>
   );
 }
