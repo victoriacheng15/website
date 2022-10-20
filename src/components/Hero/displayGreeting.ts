777385fb40f9a3b1a8d0abd3d932b07add40aad4
Victoria Cheng <victoria.cheng15@gmail.com>
2022-10-20 13:11:13 -0600

    refactor: create displayGreeting file and export the function

---
+++ src/components/Hero/displayGreeting.ts
@@ -0,0 +1,17 @@
+export const displayGreeting = () => {
+  const time = new Date().getHours();
+
+  if (time > 4 && time < 12) {
+    return "Good morning! 🌄";
+  }
+
+  if (time === 12) {
+    return "Happy Noon!";
+  }
+
+  if (time > 12 && time < 18) {
+    return "Good afternoon!";
+  }
+
+  return "Good evening! 🌇";
+};
