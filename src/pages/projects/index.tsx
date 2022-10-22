664516614d1005fa9e7d82e1fce016fcc5f679a3
Victoria Cheng <victoria.cheng15@gmail.com>
2022-10-22 16:54:34 -0600

    feat: populate project cards

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -1,11 +1,18 @@
 import React from "react";
 import Layout from "@theme/Layout";
+import ProjectCard from "@site/src/components/ProjectCard";
+import { projects } from "./projectsList";
+import styles from "./index.module.css";
 
 export default function Projects(): JSX.Element {
   return (
     <Layout title="Projects">
-      <main>
-        <h2>project</h2>
+      <main className={styles.project}>
+        <div className={styles.container}>
+          <div className={styles.project__grid}>
+            <ProjectCard projectsList={projects} />
+          </div>
+        </div>
       </main>
     </Layout>
   );
