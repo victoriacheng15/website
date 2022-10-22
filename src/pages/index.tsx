1eba340d8835ec15a70fcb548b6c3229a8dafe0d
Victoria Cheng <victoria.cheng15@gmail.com>
2022-10-21 23:54:59 -0600

    feat: add skills component to the home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import Layout from "@theme/Layout";
 import Hero from "../components/Hero";
 import SocialLinks from "../components/SocialLinks";
+import Skills from "../components/skills";
 
 function Home(): JSX.Element {
   return (
     <Layout title="Home">
       <Hero />
       <SocialLinks />
+      <Skills />
     </Layout>
   );
 }
