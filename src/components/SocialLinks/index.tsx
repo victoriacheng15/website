2ebbd98a880c77c1e198dbd5bfe9714911d4242c
Victoria Cheng <victoria.cheng15@gmail.com>
2022-10-24 16:46:39 -0600

    refactor: remove section tag and replaced ul with List

--- src/components/SocialLinks/index.tsx
+++ src/components/SocialLinks/index.tsx
@@ -1,20 +1,19 @@
 import React from "react";
 import { socialList } from "./socialList";
+import List from "../List";
 import styles from "./index.module.css";
 
 function SocialLinks() {
   return (
-    <section className="container container--fluid">
-      <ul className={styles.list}>
-        {socialList.map((site) => (
-          <li className={styles.list__link}>
-            <a href={site.link} target="_blank" rel="noopener">
-              <img alt={site.title} src={`./img/${site.title}.svg`} />
-            </a>
-          </li>
-        ))}
-      </ul>
-    </section>
+    <List classes={styles.social}>
+      {socialList.map((site) => (
+        <li className={styles.list__link}>
+          <a href={site.link} target="_blank" rel="noopener">
+            <img alt={site.title} src={`./img/${site.title}.svg`} />
+          </a>
+        </li>
+      ))}
+    </List>
   );
 }
 
