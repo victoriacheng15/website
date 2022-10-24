feef8c691d6a35622393b4e7775ff6c90ab743c9
Victoria Cheng <victoria.cheng15@gmail.com>
2022-10-23 22:42:42 -0600

    chore: move contact.tsx to page root folder

---
+++ src/pages/Contact.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Layout from "@theme/Layout";
+
+const title = "Contact";
+
+function Contact() {
+  return (
+    <Layout title={title}>
+      <main>
+        <h2>contact</h2>
+      </main>
+    </Layout>
+  );
+}
+
+export default Contact;
