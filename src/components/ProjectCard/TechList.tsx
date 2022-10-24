d11c36f1e94e211df17c17411631fd7924c2a89a
Victoria Cheng <victoria.cheng15@gmail.com>
2022-10-23 22:47:05 -0600

    chore: include classes from infima

--- src/components/ProjectCard/TechList.tsx
+++ src/components/ProjectCard/TechList.tsx
@@ -1,11 +1,15 @@
 import React from "react";
+import clsx from "clsx";
 import styles from "./index.module.css";
 
 function TechList({ techs }) {
   return (
-    <ul className={styles.techs__list}>
+    <ul className={clsx("pills", styles.techs__list)}>
       {techs.map((item) => (
-        <li key={item} className={styles.tech}>
+        <li
+          key={item}
+          className={clsx("padding-vert--xs", "padding-horiz--sm", styles.tech)}
+        >
           {item}
         </li>
       ))}
