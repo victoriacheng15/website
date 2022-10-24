629f1fcf96e3d282c26dd8c9de62f654d2dae08a
Victoria Cheng <victoria.cheng15@gmail.com>
2022-10-23 23:05:52 -0600

    fix: add alt for image and remove unused classes

--- src/components/SocialLinks/index.tsx
+++ src/components/SocialLinks/index.tsx
@@ -4,15 +4,17 @@ import styles from "./index.module.css";
 
 function SocialLinks() {
   return (
-    <ul className={styles.list}>
-      {socialList.map((site) => (
-        <li className={styles.list__link}>
-          <a href={site.link} target="_blank" rel="noopener">
-            <img src={`./img/${site.title}.svg`} />
-          </a>
-        </li>
-      ))}
-    </ul>
+    <section className="container container--fluid">
+      <ul className={styles.list}>
+        {socialList.map((site) => (
+          <li className={styles.list__link}>
+            <a href={site.link} target="_blank" rel="noopener">
+              <img alt={site.title} src={`./img/${site.title}.svg`} />
+            </a>
+          </li>
+        ))}
+      </ul>
+    </section>
   );
 }
 
