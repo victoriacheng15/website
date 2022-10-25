d31411eeed86fa5f016a5dcaf1920eb8513ff1a3
Victoria Cheng <victoria.cheng15@gmail.com>
2022-10-25 12:57:09 -0600

    feat: populate contact page with form

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,13 +1,36 @@
 import React from "react";
 import Layout from "@theme/Layout";
+import SectionTitle from "../components/SectionTitle";
+import FormField from "../components/FormField";
 
 const title = "Contact";
 
 function Contact() {
   return (
     <Layout title={title}>
-      <main>
-        <h2>contact</h2>
+      <main className="container container--fluid padding-vert--lg">
+        <div className="row">
+          <div className="col col--6 col--offset-3">
+            <SectionTitle title={`👋 ${title}`} />
+            <form id="ft-form" method="POST" accept-charset="UTF-8" action="https://form.taxi/s/v281n9zc">
+              <FormField label="Your Name:" name="name" />
+              <FormField label="Email:" name="email" />
+              <FormField
+                fieldType="textarea"
+                label="Message:"
+                name="message"
+                rows={18}
+              />
+              <input type="text" name="_gotcha" style={{display: "none"}} />
+              <button
+                type="submit"
+                className="button button--lg button--primary"
+              >
+                Send
+              </button>
+            </form>
+          </div>
+        </div>
       </main>
     </Layout>
   );
