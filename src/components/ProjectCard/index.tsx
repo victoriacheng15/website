11030f8f8422ba4ce0b4069f3570cfde2e4b788c
Victoria Cheng <victoria.cheng15@gmail.com>
2022-10-25 14:38:51 -0600

    feat: add link buttons and its style

--- src/components/ProjectCard/index.tsx
+++ src/components/ProjectCard/index.tsx
@@ -5,11 +5,18 @@ import ImageContainer from "./ImageContainer";
 import ContentTitle from "./ContentTitle";
 import TechList from "./TechList";
 
-function ProjectCard({ title, image, description, techs }) {
+function ProjectCard({ title, image, description, techs, code, demo }) {
   return (
     <div className="col col--4">
       <section className={clsx("card", styles.card)}>
-        <ImageContainer image={image} title={title} />
+        <ImageContainer
+          image={image}
+          title={title}
+          link={code}
+          name="code"
+          link2={demo}
+          name2="demo"
+        />
         <div className={clsx("card__body")}>
           <ContentTitle title="Description:" />
           <p>{description}</p>
