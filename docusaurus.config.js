a4264c26ec71a816d7f9fd7ef34c8123393076e1
Victoria Cheng <victoria.cheng15@gmail.com>
2022-10-27 20:00:57 -0600

    Revert "fix: change the docs title from resources to journal"
    This reverts commit b4416f2f285e1957a3ff4cc90589712624abe1cd.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -92,7 +92,7 @@ const config = {
             type: "doc",
             docId: "intro",
             position: "right",
-            label: "Journals",
+            label: "Resources",
           },
           { to: "/blog", label: "Blog", position: "right" },
           { to: "/contact", label: "Contact", position: "right" },
