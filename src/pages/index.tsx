2d5adcc83d121f0d2db48362f9f653f8f667b26a
Victoria Cheng <victoria.cheng15@gmail.com>
2022-11-01 16:26:33 -0600

    refactor: remove props for AboutMe

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,16 +3,13 @@ import Layout from "@theme/Layout";
 import Hero from "../components/Hero";
 import AboutMe from "../components/AboutMe";
 import Skills from "../components/skills";
-import DescAboutMe from "./assets/index/_about-me.md";
-
-const avatar = require("./assets/index/avatar.png");
 
 function Home(): JSX.Element {
   return (
     <Layout title="Home">
       <Hero />
       <main className="container container--fluid padding-vert--lg">
-        <AboutMe avatar={avatar} AboutMeDesc={<DescAboutMe />} />
+        <AboutMe />
         <Skills />
       </main>
     </Layout>
