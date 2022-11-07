f78c6e412003ccaed97a7338205ce30845182a05
Victoria Cheng <35031228+victoriacheng15@users.noreply.github.com>
2022-11-07 01:50:58 -0700

    Feat/add mermaid (#45)
    * builds: add mermaid diagram package
    
    * builds: add mermaid diagram config
    
    * feat: add mermaid diagram for outcome and identity based habits
    
    * feat: add diagram to illustrate how props passing down
    
    * styles: code has been prettified!
    
    Co-authored-by: victoriacheng15 <victoriacheng15@users.noreply.github.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -192,6 +192,10 @@ const config = {
       }),
     ],
   ],
+  markdown: {
+    mermaid: true,
+  },
+  themes: ["@docusaurus/theme-mermaid"],
 
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
