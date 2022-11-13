32b68a2eacfcd6115d336d8616f117e843bbbe76
Victoria Cheng <victoria.cheng15@gmail.com>
2022-11-13 14:53:25 -0700

    refactor: remove unused classes and SectionTitle

--- src/components/AboutMe/index.tsx
+++ src/components/AboutMe/index.tsx
@@ -1,21 +1,31 @@
 import React from "react";
 import clsx from "clsx";
-import SectionTitle from "../SectionTitle";
 import DescAboutMe from "./_about-me.md";
 import styles from "./index.module.css";
 
 function About() {
   return (
     <section className="container container--fluid">
-      <SectionTitle title="About Me" />
       <div className="row">
         <img
-          className={clsx("col", "col--4", "col--offset-1", styles.avatar)}
+          className={clsx(
+            "col",
+            "col--4",
+            "col--offset-1",
+            "margin-bottom--lg",
+            styles.avatar
+          )}
           src="./img/avatar.webp"
           alt="Victoria with orange background"
         />
         <section
-          className={clsx("col", "col--6", "col--offset-1", styles.para)}
+          className={clsx(
+            "col",
+            "col--6",
+            "col--offset-1",
+            "keep",
+            styles.para
+          )}
         >
           <DescAboutMe />
         </section>
