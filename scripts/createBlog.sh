013764d61d45dfacfb817b1a932ae0011f356f81
Victoria Cheng <35031228+victoriacheng15@users.noreply.github.com>
2022-12-06 22:44:49 -0700

    feat: add a bash script to create blog template (#107)

---
+++ scripts/createBlog.sh
@@ -0,0 +1,47 @@
+#! /usr/bin/bash
+
+read -p "Enter your title: " title
+read -p "Enter tags: " tags
+read -p "Enter image alt: " alt
+read -p "Enter month: " mm
+read -p "Enter day: " dd
+
+slug=$(echo $title | tr '[:upper:]' '[:lower:]' | tr ' ' '-')
+formatted_alt=$(echo $alt | tr '[:upper:]' '[:lower:]' | tr ' ' '-')
+
+year=$(date +"%Y")
+month=${mm:-$(date +"%m")}
+day=${dd:-$(date +"%d")}
+
+folder=$month-$day-$slug
+
+function createBlog() {
+  mkdir $folder
+
+  cd $folder
+
+  touch index.md
+
+  echo --- >> index.md
+  echo slug: $slug >> index.md
+  echo title: $title >> index.md
+  echo author: victoria >> index.md
+  echo tags: [$tags] >> index.md
+  echo hide_table_of_contents: false >> index.md
+  echo --- >> index.md 
+  echo -e "\n" >> index.md
+  echo "![$alt](./$formatted_alt.webp)" >> index.md
+  echo -e "\n" >> index.md 
+  echo "<!--truncate-->" >> index.md
+}
+
+if test -d $year; then
+  cd $year
+
+  createBlog
+else
+  mkdir $year
+  cd $year
+
+  createBlog
+fi
