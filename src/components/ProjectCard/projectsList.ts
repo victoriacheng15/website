cf892801016a882fa2d87e2216d1cb996d09fc0c
Victoria Cheng <victoria.cheng15@gmail.com>
2023-01-10 13:26:02 -0700

    feat: create projectsList.ts and store projects info

---
+++ src/components/ProjectCard/projectsList.ts
@@ -0,0 +1,35 @@
+export const projectsList = [
+	{
+		title: "Bubble Tea App",
+		code: "https://github.com/victoriacheng15/bubble-tea-api#readme",
+		demo: "https://bubble-tea.up.railway.app/",
+		image: "./img/projects/bubble-tea-app.webp",
+		description:
+			"Developed an app where users can submit their favorite bubble tea combinations and view a leaderboard that ranks the popularity of these combinations. Whether you're a bubble tea aficionado or just looking for some inspiration for your next drink, this app is sure to satisfy your cravings.",
+		techs: ["Express", "JavaScript", "MongoDB", "CSS"],
+	},
+	{
+		title: "Country Information App",
+		code: "https://github.com/victoriacheng15/rest-countries-api-ts#readme",
+		demo: "https://rest-countries-api-ts-vc.netlify.app/",
+		image: "./img/projects/country-information-app.webp",
+		description:
+			"Developed an app that displays short information about various countries on cards, with pagination to navigate between them. Each card leads to a separate page with more detailed information about the corresponding country. This app is a convenient and easy way to learn about the world around us.",
+		techs: [
+			"TypeScript",
+			"React",
+			"React Router",
+			"Tailwind CSS",
+			"Testing Library",
+		],
+	},
+	{
+		title: "Space Tourism Website",
+		code: "https://github.com/victoriacheng15/fem-space-tourism#readme",
+		demo: "https://fem-space-tourism-vc.vercel.app/",
+		image: "./img/projects/space-tourism-website.webp",
+		description:
+			"Our tourism website showcases information about various destinations, the crew, and the technologies available for an out-of-this-world space travel experience. Explore our website to learn more about the exciting opportunities that await you on your journey.",
+		techs: ["React", "React Router", "Tailwind CSS"],
+	},
+];
